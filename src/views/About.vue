1be50efc12f1542c9c3cf561164c148136657a55
devwithu <devwithu@naver.com>
2022-01-20 10:56:26 +0900

    3-4 Get Pages Ready

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus dicta, ea esse eveniet excepturi, expedita fuga in, iste laborum maxime minima nobis quos recusandae repellendus tempore temporibus velit voluptate voluptatem!
+    </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio eaque eius error ex fugit incidunt laborum, minus modi nam natus necessitatibus odio placeat porro quo rem repellat, tempore veniam voluptas.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio eaque eius error ex fugit incidunt laborum, minus modi nam natus necessitatibus odio placeat porro quo rem repellat, tempore veniam voluptas.</p>
+
   </div>
 </template>
